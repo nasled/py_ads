eb0cc6bee5144877dbbc8bc3119a4f149e81b03c
artsiom <art.naslednikau@gmail.com>
2022-05-17 22:38:52 -0400

    improved remove operation of min_heap data structure

--- data_structure/min_heap.py
+++ data_structure/min_heap.py
@@ -1,6 +1,3 @@
-from algorithm.bubble_sort import bubble_sort
-
-
 class MinHeap:
     def __init__(self):
         self.count = 0
@@ -19,7 +16,6 @@ class MinHeap:
 
     def min_heapify(self):
         i = self.count - 1
-
         while i > 0 and self.heap_list[int((i-1)/2)] > self.heap_list[i]:
             self.swap(self.heap_list[i], self.heap_list[int((i-1)/2)])
             i = int((i - 1) / 2)
@@ -35,7 +31,17 @@ class MinHeap:
             value_index = self.heap_list.index(value)
             self.count = self.count - 1
             self.heap_list[value_index] = self.heap_list[self.count]
-            self.heap_list = bubble_sort(self.heap_list)
+            min_value = float('Infinity')
+            min_index = -1
+            for current_index in range(self.count):
+                if self.count > current_index + 1:
+                    if min_value > self.heap_list[current_index]:
+                        min_index = current_index
+                        min_value = self.heap_list[current_index]
+            if min_index >= 0:
+                temp_value = self.heap_list[0]
+                self.heap_list[0] = self.heap_list[min_index]
+                self.heap_list[min_index] = temp_value
             return True
         else:
             return False
@@ -47,7 +53,10 @@ class MinHeap:
         return True if self.count > index else False
 
     def peek(self):
-        return self.heap_list[0]
+        if self.count:
+            return self.heap_list[0]
+        else:
+            return -1
 
 
 if __name__ == "__main__":
@@ -84,3 +93,17 @@ if __name__ == "__main__":
     h2.remove(140)
     assert h2.contains(140) is False
     assert h2.count == 0
+
+    h3 = MinHeap()
+    h3.add(5)
+    h3.add(6)
+    h3.add(7)
+    h3.add(8)
+    h3.remove(5)
+    assert h3.peek() == 6
+    h3.remove(6)
+    assert h3.peek() == 7
+    h3.remove(7)
+    assert h3.peek() == 8
+    h3.remove(8)
+    assert h3.peek() == -1
